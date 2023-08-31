5086b72b0a7395abf8ca4a29d806748e49d7cf64
Anant Narayan <anantnrg@gmail.com>
2023-08-31 16:17:49 +0530

    added new function to WinitData

--- src/libs/structs/backends.rs
+++ src/libs/structs/backends.rs
@@ -10,3 +10,12 @@ pub struct WinitData {
 	pub backend: WinitGraphicsBackend<GlowRenderer>,
 	pub damage_tracker: OutputDamageTracker,
 }
+
+impl WinitData {
+	pub fn new(
+		backend: WinitGraphicsBackend<GlowRenderer>,
+		damage_tracker: OutputDamageTracker,
+	) -> Self {
+		Self { backend, damage_tracker }
+	}
+}
