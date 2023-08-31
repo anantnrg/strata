cf13233e02e05fdb90a3c9a0214cd4544ebc6b8d
Anant Narayan <anantnrg@gmail.com>
2023-08-31 16:22:13 +0530

    used new function in winit.rs. still can't figure out what to pass to StrataState

--- src/libs/backends/winit.rs
+++ src/libs/backends/winit.rs
@@ -84,7 +84,7 @@ pub fn init_winit() {
 	output.change_current_state(Some(mode), Some(Transform::Flipped180), None, Some((0, 0).into()));
 	output.set_preferred(mode);
 	let damage_tracked_renderer = OutputDamageTracker::from_output(&output);
-	let winitdata = Box::new(WinitData { backend, damage_tracker: damage_tracked_renderer });
+	let winitdata = Box::new(WinitData::new(backend, damage_tracked_renderer));
 	let state =
 		StrataState::new(event_loop.handle(), event_loop.get_signal(), &mut display, winitdata);
 
